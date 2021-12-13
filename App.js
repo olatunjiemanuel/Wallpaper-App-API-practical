8bb6a8427717ef8b19c846356bf27cb9be4146e4
olatunjiemanuel <93155336+olatunjiemanuel@users.noreply.github.com>
2021-12-13 16:13:43 +0100

    working on UI

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      images: [],
+    };
+    this.LoadWallpapers = this.loadWallpapers.bind(this);
+  }
+
+  loadWallpapers() {
+    axios
+      .get(
+        "https://api.unsplash.com/photos/random?count=30&client_id=g-W2R8DnWaizl6ZSEW2LBaReApCeA38eYHoqLe9AqGI"
+      )
+      .then(function (response) {
+        console.log(response.description);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        console.log("request completed");
+      });
+  }
+
+  componentDidMount() {
+    this.LoadWallpapers;
+  }
+
+  render() {
+    return this.state.isLoading ? (
+      <View
+        style={{
+          backgroundColor: "black",
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator size="large" color="gray" />
+      </View>
+    ) : (
+      <View style={{ flex: 1, backgroundColor: "black" }}></View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
